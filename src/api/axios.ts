1cb94ecd2977e43c84d672a03daa5c2b717fd303
Elisangela Miranda dos Anjos <50150279+elisangela10@users.noreply.github.com>
2026-03-21 14:37:09 -0300

    ajuste para chamada api PRD

--- src/api/axios.ts
+++ src/api/axios.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "https://localhost:7218/api", // Ajustado para desenvolvimento local
+  baseURL: "https://casadeaxe-api.onrender.com/api", // Ajustado para desenvolvimento local
 });
 
 export default api;
